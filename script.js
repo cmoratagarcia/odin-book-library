e67a1a520069fc9c7728e1692c04ef15a1d519ec
Carmen Morata <cmoratagarcia@gmail.com>
2024-01-09 09:51:54 -0800

    Add skeleton files

---
+++ script.js
@@ -0,0 +1,13 @@
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.info = function () {
+    return `${this.title}, ${this.author}, ${this.pages}, ${this.read}`;
+  };
+}
+
+let hobbit = new Book("The Hobbit", "Tolkien", 500, true);
+
+console.log(hobbit.info());
